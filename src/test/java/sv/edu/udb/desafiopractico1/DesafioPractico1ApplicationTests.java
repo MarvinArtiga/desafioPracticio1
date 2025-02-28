6258f0d4d6c6e624068d6536715c8ec909f8d37a
Marvin Artiga <marvin.artigagonzalez@gmail.com>
2025-02-27 23:37:23 -0600

    initial commit

---
+++ src/test/java/sv/edu/udb/desafiopractico1/DesafioPractico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafiopractico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPractico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
